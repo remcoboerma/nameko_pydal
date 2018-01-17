e49d8255ff885c2794ac21c7ffd14fc910d16af7
Remco Boerma <remcoboerma@users.noreply.github.com>
2018-01-17 14:26:41 +0100

    Uses thread local context, tries commit/rollback
    Automatic commit and rollback are applied whether an exception is raised. 
    Connections are closed even better in the 'strange' event that stop isn't sufficient. 
    An extra parameter is issued based on the config file ('other_args') to pass arguments from the config file to the define_model function.

--- pydal_extension.py
+++ pydal_extension.py
@@ -1,5 +1,10 @@
+from __future__ import print_function
 import pydal
 from nameko.extensions import DependencyProvider
+
+# import threading
+# TL = THREAD_LOCAL = threading.local()
+# import uuid
 
 # for sqlite: pip install pypiwin32 on windows!
 # see http://nameko.readthedocs.org/en/stable/writing_extensions.html
@@ -14,33 +19,42 @@ class DalProvider(DependencyProvider):
         Create a new pydal depencency provider.
         Use the callback to define the database model on container start.
 
-        :param define_model: a callback with the opened pydal connetion as a parameter.
+        :param define_model: a callback with the opened pydal connetion, other_args as parameters
         :return:
         """
         super(DalProvider, self).__init__()
         self.define_model = define_model
+        # print('DalProvider::__init__')
 
-    def setup(self):
+
+    def worker_setup(self, worker_ctx):
         """
         Read from database_uris section in the config file the service_name as a key
-        :return:
+        :return: db_connection
         """
+        # thread_id = uuid.uuid4()
+        # TL.id = thread_id
+        # print('DalProvider::worker_setup', getattr(TL,'id','?'))
         db_config = self.container.config[DB_URIS][self.container.service_name]
         self.db_args = db_config['args']
         self.db_kwargs = db_config['kwargs']
-
-    def start(self):
-        self.db_connection = pydal.DAL(*self.db_args, **self.db_kwargs)
-        if callable(self.define_model): self.define_model(self.db_connection)
+        self.other_args = db_config.get('other_args',{})
+        worker_ctx.db_connection = pydal.DAL(*self.db_args, **self.db_kwargs)
+        if callable(self.define_model):
+            self.define_model(worker_ctx.db_connection, self.other_args)
 
     def get_dependency(self, worker_ctx):
-        return self.db_connection
+        if not hasattr(worker_ctx,'db_connection'):
+            self.worker_setup(worker_ctx)
+        return worker_ctx.db_connection
 
-    def stop(self):
-        self.db_connection.close()
+    def worker_teardown(self, worker_ctx):
+        # print('DalProvider::worker_teardown', getattr(TL,'id','?'))
+        try:
+            worker_ctx.db_connection.close()
+        except:
+            pass
 
-    def kill(self):
-        self.db_connection.rollback()
 
     def worker_result(self, worker_ctx, result=None, exc_info=None):
         """
@@ -55,8 +69,15 @@ class DalProvider(DependencyProvider):
         :param exc_info:
         :return:
         """
+        # print('DalProvider::worker_result')
         if exc_info is not None:
-            self.db_connection.rollback()
+            try:
+                worker_ctx.db_connection.rollback()
+            except:
+                pass
         else:
-            self.db_connection.commit()
+            try:
+                worker_ctx.db_connection.commit()
+            except:
+                pass
 
